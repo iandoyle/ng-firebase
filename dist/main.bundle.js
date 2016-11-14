0cceeabd5f5b670a65ccfd17188ea8499b932ae6
iandoyle <get@iandoyle.pro>
2016-11-14 13:55:34 -0500

    and down

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -39717,9 +39717,7 @@ var AppComponent = (function () {
         });
     };
     AppComponent.prototype.changeState = function (state, key) {
-        console.log('changing state to: ' + state);
         if (key) {
-            console.log('changing key to: ' + key);
             this.activeKey = key;
         }
         this.appState = state;
@@ -39729,6 +39727,58 @@ var AppComponent = (function () {
         this._firebaseService.getBusinesses(category).subscribe(function (businesses) {
             _this.businesses = businesses;
         });
+    };
+    AppComponent.prototype.addBusiness = function (company, category, years_in_business, description, phone, email, street_address, city, state, zipcode) {
+        var created_at = new Date().toString();
+        var newBusiness = {
+            company: company,
+            category: category,
+            years_in_business: years_in_business,
+            description: description,
+            phone: phone,
+            email: email,
+            street_address: street_address,
+            city: city,
+            state: state,
+            zipcode: zipcode,
+            created_at: created_at
+        };
+        //console.log(newBusiness);
+        this._firebaseService.addBusiness(newBusiness);
+        this.changeState('default', null);
+    };
+    AppComponent.prototype.showEdit = function (business) {
+        this.changeState('edit', business.$key);
+        this.activeCompany = business.company;
+        this.activeCategory = business.category;
+        this.activeYearsInBusiness = business.years_in_business;
+        this.activeDescription = business.description;
+        this.activePhone = business.phone;
+        this.activeEmail = business.email;
+        this.activeStreetAddress = business.street_address;
+        this.activeCity = business.city;
+        this.activeState = business.state;
+        this.activeZipcode = business.zipcode;
+    };
+    AppComponent.prototype.updateBusiness = function () {
+        var updBusiness = {
+            company: this.activeCompany,
+            category: this.activeCategory,
+            years_in_business: this.activeYearsInBusiness,
+            description: this.activeDescription,
+            phone: this.activePhone,
+            email: this.activeEmail,
+            street_address: this.activeStreetAddress,
+            city: this.activeCity,
+            state: this.activeState,
+            zipcode: this.activeZipcode
+        };
+        this._firebaseService.updateBusiness(this.activeKey, updBusiness);
+        this.changeState('default', null);
+    };
+    AppComponent.prototype.deleteBusiness = function (key) {
+        this._firebaseService.deleteBusiness(key);
+        this.changeState('default', null);
     };
     AppComponent = __decorate([
         __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["F" /* Component */])({
@@ -53250,6 +53300,15 @@ var firebaseService = (function () {
         this.categories = this._af.database.list('/categories');
         return this.categories;
     };
+    firebaseService.prototype.addBusiness = function (newBusiness) {
+        return this.businesses.push(newBusiness);
+    };
+    firebaseService.prototype.updateBusiness = function (key, updBusiness) {
+        return this.businesses.update(key, updBusiness);
+    };
+    firebaseService.prototype.deleteBusiness = function (key) {
+        return this.businesses.remove(key);
+    };
     firebaseService = __decorate([
         __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["d" /* Injectable */])(), 
         __metadata('design:paramtypes', [(typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1_angularfire2__["a" /* AngularFire */] !== 'undefined' && __WEBPACK_IMPORTED_MODULE_1_angularfire2__["a" /* AngularFire */]) === 'function' && _a) || Object])
@@ -56978,7 +57037,7 @@ module.exports = ""
 /* 625 */
 /***/ function(module, exports) {
 
-module.exports = "<div class=\"top-bar\">\n  <div class=\"row\">\n    <div class=\"top-bar-left\">\n      <ul class=\"menu\">\n        <li class=\"menu-text\">Business Contacts</li>\n      </ul>\n    </div>\n    <div class=\"top-bar-right\">\n\n    </div>\n  </div>\n</div>\n<br>\n\n<div class=\"row\">\n  <div class=\"large-6 columns\">\n    <button (click)=\"changeState('add')\" class=\"button\">Add Business</button>\n  </div>\n  <div class=\"large-6 columns\">\n    <label> Filter Category\n        <select (change)=\"filterCategory(filteredCategory.value)\" #filteredCategory>\n          <option value=\"0\">Select</option>\n          <option *ngFor=\"let category of categories\" value=\"{{category.name}}\">{{category.name}}</option>\n        </select>\n    </label>\n  </div>\n</div>\n\n<div *ngIf=\"appState == 'extend'\">\n  <div class=\"row\">\n    <div class=\"large-12 columns\">\n      <button (click)=\"changeState('default')\" class=\"button alert\">Go Back</button>\n    </div>\n  </div>\n\n  <div *ngFor=\"let business of businesses\">\n    <div *ngIf=\"business.$key == activeKey\">\n      <div class=\"row\">\n        <div class=\"large-5 columns large-offset-1\">\n          <h3>{{business.company}}</h3>\n          <p>Category: {{business.category}}</p>\n          <p>Years In Business: {{business.years_in_business}}</p>\n          <p>{{business.description}}</p>\n        </div>\n        <div class=\"large-5 columns large-offset-1\">\n          <h4>Contact Info</h4>\n          <ul>\n            <li>Phone: {{business.phone}}</li>\n            <li>Email: {{business.email}}</li>\n            <li>Street: {{business.street_address}}</li>\n            <li>City: {{business.city}}</li>\n            <li>State: {{business.state}}</li>\n            <li>Zipcode: {{business.zipcode}}</li>\n          </ul>\n        </div>\n      </div>\n    </div>\n  </div>\n</div>\n\n<div class=\"row\">\n  <div class=\"large-12 columns\">\n    <div *ngIf=\"businesses\">\n      <table>\n        <thead>\n          <tr>\n            <th>Company</th>\n            <th>Category</th>\n            <th>Phone</th>\n            <th></th>\n          </tr>\n        </thead>\n        <tbody>\n          <tr *ngFor=\"let business of businesses\">\n            <td>{{business.company}}</td>\n            <td>{{business.category}}</td>\n            <td>{{business.phone}}</td>\n            <td>\n              <button (click)=\"changeState('extend', business.$key)\" class=\"button\">More</button>\n              <button class=\"button secondary\">Edit</button>\n              <button class=\"button alert\">Delete</button>\n            </td>\n          </tr>\n        </tbody>\n      </table>\n    </div>\n  </div>\n</div>"
+module.exports = "<div class=\"top-bar\">\n  <div class=\"row\">\n    <div class=\"top-bar-left\">\n      <ul class=\"menu\">\n        <li class=\"menu-text\">Business Contacts</li>\n      </ul>\n    </div>\n    <div class=\"top-bar-right\">\n\n    </div>\n  </div>\n</div>\n<br>\n\n<div class=\"row\">\n  <div class=\"large-6 columns\">\n    <button (click)=\"changeState('add')\" class=\"button\">Add Business</button>\n  </div>\n  <div class=\"large-6 columns\">\n    <label> Filter Category\n        <select (change)=\"filterCategory(filteredCategory.value)\" #filteredCategory>\n          <option value=\"0\">Select</option>\n          <option *ngFor=\"let category of categories\" value=\"{{category.name}}\">{{category.name}}</option>\n        </select>\n    </label>\n  </div>\n</div>\n\n\n<!-- Add Form -->\n<div *ngIf=\"appState == 'add'\" class=\"row\">\n  <div class=\"large-12 columns\">\n    <h3>Add Business</h3>\n    <form (submit)=\"addBusiness(\n      company.value,\n      category.value,\n      years_in_business.value,\n      description.value,\n      phone.value,\n      email.value,\n      street_address.value,\n      city.value,\n      state.value,\n      zipcode.value)\">\n      <div class=\"row\">\n        <div class=\"large-6 columns\">\n          <label>Company\n            <input type=\"text\" placeholder=\"Company Name\" #company>\n          </label>\n        </div>\n        <div class=\"large-6 columns\">\n          <label>Category\n            <select #category>\n              <option value=\"0\">Select</option>\n              <option *ngFor=\"let category of categories\" value=\"{{category.name}}\">{{category.name}}</option>\n            </select>\n          </label>\n        </div>\n      </div>\n\n      <div class=\"row\">\n        <div class=\"medium-6 columns\">\n          <label>Years In Business\n            <input type=\"text\" placeholder=\"Years In Business\" #years_in_business>\n          </label>\n        </div>\n        <div class=\"medium-6 columns\">\n          <label>Description\n            <textarea #description></textarea>\n          </label>\n        </div>\n      </div>\n\n      <div class=\"row\">\n        <div class=\"medium-6 columns\">\n          <label>Phone\n            <input type=\"text\" placeholder=\"Phone\" #phone>\n          </label>\n        </div>\n        <div class=\"medium-6 columns\">\n          <label>Email\n            <input type=\"text\" placeholder=\"Email\" #email>\n          </label>\n        </div>\n      </div>\n\n      <div class=\"row\">\n        <div class=\"medium-6 columns\">\n          <label>Street Address\n            <input type=\"text\" placeholder=\"Street Address\" #street_address>\n          </label>\n        </div>\n        <div class=\"medium-6 columns\">\n          <label>City\n            <input type=\"text\" placeholder=\"City\" #city>\n          </label>\n        </div>\n      </div>\n\n      <div class=\"row\">\n        <div class=\"medium-6 columns\">\n          <label>State\n            <input type=\"text\" placeholder=\"State\" #state>\n          </label>\n        </div>\n        <div class=\"medium-6 columns\">\n          <label>Zipcode\n            <input type=\"text\" placeholder=\"Zipcode\" #zipcode>\n          </label>\n        </div>\n      </div>\n\n      <div class=\"row\">\n        <div class=\"large-12\">\n          <input type=\"submit\" class=\"button\" value=\"Submit\">\n        </div>\n      </div>\n\n    </form>\n  </div>\n</div>\n\n<!-- Edit Form -->\n<div *ngIf=\"appState == 'edit'\" class=\"row\">\n  <div class=\"large-12 columns\">\n    <h3>Edit Business</h3>\n    <form (submit)=\"updateBusiness()\">\n      <div class=\"row\">\n        <div class=\"large-6 columns\">\n          <label>Company\n            <input type=\"text\" [(ngModel)]=\"activeCompany\" name=\"activeCompany\" placeholder=\"Company Name\" #company>\n          </label>\n        </div>\n        <div class=\"large-6 columns\">\n          <label>Category\n            <select [(ngModel)]=\"activeCategory\" name=\"activeCategory\" #category>\n              <option value=\"0\">Select</option>\n              <option *ngFor=\"let category of categories\" value=\"{{category.name}}\">{{category.name}}</option>\n            </select>\n          </label>\n        </div>\n      </div>\n\n      <div class=\"row\">\n        <div class=\"medium-6 columns\">\n          <label>Years In Business\n            <input type=\"text\" [(ngModel)]=\"activeYearsInBusiness\" name=\"activeYearsInBusiness\" placeholder=\"Years In Business\" #years_in_business>\n          </label>\n        </div>\n        <div class=\"medium-6 columns\">\n          <label>Description\n            <textarea [(ngModel)]=\"activeDescription\" name=\"activeDescription\" #description></textarea>\n          </label>\n        </div>\n      </div>\n\n      <div class=\"row\">\n        <div class=\"medium-6 columns\">\n          <label>Phone\n            <input type=\"text\" [(ngModel)]=\"activePhone\" name=\"activePhone\" placeholder=\"Phone\" #phone>\n          </label>\n        </div>\n        <div class=\"medium-6 columns\">\n          <label>Email\n            <input type=\"text\" [(ngModel)]=\"activeEmail\" name=\"activeEmail\" placeholder=\"Email\" #email>\n          </label>\n        </div>\n      </div>\n\n      <div class=\"row\">\n        <div class=\"medium-6 columns\">\n          <label>Street Address\n            <input type=\"text\" [(ngModel)]=\"activeStreetAddress\" name=\"activeStreetAddress\" placeholder=\"Street Address\" #street_address>\n          </label>\n        </div>\n        <div class=\"medium-6 columns\">\n          <label>City\n            <input type=\"text\" [(ngModel)]=\"activeCity\" name=\"activeCity\" placeholder=\"City\" #city>\n          </label>\n        </div>\n      </div>\n\n      <div class=\"row\">\n        <div class=\"medium-6 columns\">\n          <label>State\n            <input type=\"text\" [(ngModel)]=\"activeState\" name=\"activeState\" placeholder=\"State\" #state>\n          </label>\n        </div>\n        <div class=\"medium-6 columns\">\n          <label>Zipcode\n            <input type=\"text\" [(ngModel)]=\"activeZipcode\" name=\"activeZipcode\" placeholder=\"Zipcode\" #zipcode>\n          </label>\n        </div>\n      </div>\n\n      <div class=\"row\">\n        <div class=\"large-12\">\n          <input type=\"submit\" class=\"button\" value=\"Submit\">\n        </div>\n      </div>\n\n    </form>\n  </div>\n</div>\n\n<div *ngIf=\"appState == 'extend'\">\n  <div class=\"row\">\n    <div class=\"large-12 columns\">\n      <button (click)=\"changeState('default')\" class=\"button alert\">Go Back</button>\n    </div>\n  </div>\n\n  <div *ngFor=\"let business of businesses\">\n    <div *ngIf=\"business.$key == activeKey\">\n      <div class=\"row\">\n        <div class=\"large-5 columns large-offset-1\">\n          <h3>{{business.company}}</h3>\n          <p>Category: {{business.category}}</p>\n          <p>Years In Business: {{business.years_in_business}}</p>\n          <p>{{business.description}}</p>\n        </div>\n        <div class=\"large-5 columns large-offset-1\">\n          <h4>Contact Info</h4>\n          <ul>\n            <li>Phone: {{business.phone}}</li>\n            <li>Email: {{business.email}}</li>\n            <li>Street: {{business.street_address}}</li>\n            <li>City: {{business.city}}</li>\n            <li>State: {{business.state}}</li>\n            <li>Zipcode: {{business.zipcode}}</li>\n          </ul>\n        </div>\n      </div>\n    </div>\n  </div>\n</div>\n\n<div class=\"row\">\n  <div class=\"large-12 columns\">\n    <div *ngIf=\"businesses\">\n      <table>\n        <thead>\n          <tr>\n            <th>Company</th>\n            <th>Category</th>\n            <th>Phone</th>\n            <th></th>\n          </tr>\n        </thead>\n        <tbody>\n          <tr *ngFor=\"let business of businesses\">\n            <td>{{business.company}}</td>\n            <td>{{business.category}}</td>\n            <td>{{business.phone}}</td>\n            <td>\n              <button (click)=\"changeState('extend', business.$key)\" class=\"button\">More</button>\n              <button (click)=\"showEdit(business)\" class=\"button secondary\">Edit</button>\n              <button (click)=\"deleteBusiness(business.$key)\" class=\"button alert\">Delete</button>\n            </td>\n          </tr>\n        </tbody>\n      </table>\n    </div>\n  </div>\n</div>"
 
 /***/ },
 /* 626 */
